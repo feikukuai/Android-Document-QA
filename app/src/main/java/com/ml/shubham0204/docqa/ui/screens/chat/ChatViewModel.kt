c6b26dd92a54da411d089cc842c4266f6ad9e475
frihu <127482729+feikukuai@users.noreply.github.com>
2025-02-19 17:30:38 +0800

    更新 ChatViewModel.kt

--- app/src/main/java/com/ml/shubham0204/docqa/ui/screens/chat/ChatViewModel.kt
+++ app/src/main/java/com/ml/shubham0204/docqa/ui/screens/chat/ChatViewModel.kt
@@ -1,47 +1,147 @@
-import kotlinx.coroutines.TimeoutCancellationException
-import kotlinx.coroutines.withTimeout
+package com.ml.shubham0204.docqa.ui.screens.docs
 
-fun getAnswer(
-    query: String,
-    prompt: String,
-) {
-    val apiKey = geminiAPIKey.getAPIKey() ?: throw Exception("Gemini API key is null")
-    val geminiRemoteAPI = GeminiRemoteAPI(apiKey)
-    _isGeneratingResponseState.value = true
-    _questionState.value = query
-    try {
-        var jointContext = ""
-        val retrievedContextList = ArrayList<RetrievedContext>()
-        val queryEmbedding = sentenceEncoder.encodeText(query)
-        chunksDB.getSimilarChunks(queryEmbedding, n = 5).forEach {
-            jointContext += " " + it.second.chunkData
-            retrievedContextList.add(RetrievedContext(it.second.docFileName, it.second.chunkData))
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import com.ml.shubham0204.docqa.data.Chunk
+import com.ml.shubham0204.docqa.data.ChunksDB
+import com.ml.shubham0204.docqa.data.Document
+import com.ml.shubham0204.docqa.data.DocumentsDB
+import com.ml.shubham0204.docqa.domain.embeddings.SentenceEmbeddingProvider
+import com.ml.shubham0204.docqa.domain.readers.Readers
+import com.ml.shubham0204.docqa.domain.splitters.WhiteSpaceSplitter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import org.koin.android.annotation.KoinViewModel
+import setProgressDialogText
+import java.io.File
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
+import kotlin.math.min
+
+@KoinViewModel
+class DocsViewModel(
+    private val documentsDB: DocumentsDB,
+    private val chunksDB: ChunksDB,
+    private val sentenceEncoder: SentenceEmbeddingProvider,
+) : ViewModel() {
+    suspend fun addDocument(
+        inputStream: InputStream,
+        fileName: String,
+        documentType: Readers.DocumentType,
+    ) = withContext(Dispatchers.IO) {
+        val text =
+            Readers.getReaderForDocType(documentType).readFromInputStream(inputStream)
+                ?: return@withContext
+        val newDocId =
+            documentsDB.addDocument(
+                Document(
+                    docText = text,
+                    docFileName = fileName,
+                    docAddedTime = System.currentTimeMillis(),
+                ),
+            )
+        setProgressDialogText("Creating chunks...")
+        val chunks = WhiteSpaceSplitter.createChunks(text, chunkSize = 500, chunkOverlap = 50)
+        setProgressDialogText("Adding chunks to database...")
+        val size = chunks.size
+        chunks.forEachIndexed { index, s ->
+            setProgressDialogText("Added ${index + 1}/$size chunk(s) to database...")
+            val embedding = sentenceEncoder.encodeText(s)
+            chunksDB.addChunk(
+                Chunk(
+                    docId = newDocId,
+                    docFileName = fileName,
+                    chunkData = s,
+                    chunkEmbedding = embedding,
+                ),
+            )
         }
-        val inputPrompt = prompt.replace("\$CONTEXT", jointContext).replace("\$QUERY", query)
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                // 设置超时时间为10秒
-                val response = withTimeout(10000) {
-                    geminiRemoteAPI.getResponse(inputPrompt)
+    }
+
+    suspend fun addDocumentFromUrl(
+        url: String,
+        context: Context,
+        onDownloadComplete: (Boolean) -> Unit,
+    ) = withContext(Dispatchers.IO) {
+        try {
+            val connection = URL(url).openConnection() as HttpURLConnection
+            connection.connect()
+            if (connection.responseCode == HttpURLConnection.HTTP_OK) {
+                val inputStream = connection.inputStream
+                val fileName = getFileNameFromURL(url)
+                val file = File(context.cacheDir, fileName)
+
+                file.outputStream().use { outputStream ->
+                    inputStream.copyTo(outputStream)
                 }
-                response?.let { llmResponse ->
-                    _responseState.value = llmResponse
-                    _isGeneratingResponseState.value = false
-                    _retrievedContextListState.value = retrievedContextList
+
+                // Determine the document type based on the file extension
+                // Add handle for unknown types if supported
+                val documentType =
+                    when (fileName.substringAfterLast(".", "").lowercase()) {
+                        "pdf" -> Readers.DocumentType.PDF
+                        "docx" -> Readers.DocumentType.MS_DOCX
+                        "doc" -> Readers.DocumentType.MS_DOCX
+                        else -> Readers.DocumentType.UNKNOWN
+                    }
+
+                // Pass file to your document handling logic
+                addDocument(file.inputStream(), fileName, documentType)
+
+                withContext(Dispatchers.Main) {
+                    onDownloadComplete(true)
                 }
-            } catch (e: TimeoutCancellationException) {
-                // 超时后的处理逻辑
-                _isGeneratingResponseState.value = false
-                _responseState.value = "请求超时，请稍后再试"
-                // 可以在这里执行其他超时后的操作
-            } catch (e: Exception) {
-                _isGeneratingResponseState.value = false
-                _responseState.value = "发生错误: ${e.message}"
+            } else {
+                withContext(Dispatchers.Main) {
+                    onDownloadComplete(false)
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            withContext(Dispatchers.Main) {
+                onDownloadComplete(false)
             }
         }
-    } catch (e: Exception) {
-        _isGeneratingResponseState.value = false
-        _questionState.value = ""
-        throw e
+    }
+
+    fun getAllDocuments(): Flow<List<Document>> = documentsDB.getAllDocuments()
+
+    fun removeDocument(docId: Long) {
+        documentsDB.removeDocument(docId)
+        chunksDB.removeChunks(docId)
+    }
+
+    fun getDocsCount(): Long = documentsDB.getDocsCount()
+
+    // Extracts the file name from the URL
+    // Source: https://stackoverflow.com/a/11576046/13546426
+    private fun getFileNameFromURL(url: String?): String {
+        if (url == null) {
+            return ""
+        }
+        try {
+            val resource = URL(url)
+            val host = resource.host
+            if (host.isNotEmpty() && url.endsWith(host)) {
+                return ""
+            }
+        } catch (e: MalformedURLException) {
+            return ""
+        }
+        val startIndex = url.lastIndexOf('/') + 1
+        val length = url.length
+        var lastQMPos = url.lastIndexOf('?')
+        if (lastQMPos == -1) {
+            lastQMPos = length
+        }
+        var lastHashPos = url.lastIndexOf('#')
+        if (lastHashPos == -1) {
+            lastHashPos = length
+        }
+        val endIndex = min(lastQMPos.toDouble(), lastHashPos.toDouble()).toInt()
+        return url.substring(startIndex, endIndex)
     }
 }
